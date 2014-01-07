e3a9ea3f87e634d6012b091e8ca90a885d7cdd7b
Plinio Balduino <pbalduino+github@gmail.com>
2014-01-07 11:51:25 -0200

    Created library skeleton

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject limonada "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[limon-model "0.1.0-SNAPSHOT"]])
